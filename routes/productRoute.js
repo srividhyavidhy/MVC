d0b792ac7ef2b10b066fcdfb66a663ccdc9d1082
srividhyavidhy <srividhyav44@gmail.com>
2023-12-01 10:49:21 +0530

    ecommerce-api

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,11 +8,11 @@ product_route.use(bodyParser.urlencoded({extended:true}));
 const auth = require("../middleware/auth");
 const product_controller = require("../controllers/productController");
 
-product_route.post('/add-product',auth,product_controller.add_product);
-product_route.get('/get-products',auth,product_controller.getProducts);
-product_route.get('/product/:productId',auth,product_controller.getProduct);
-product_route.put('/update/:_id',auth,product_controller.updateProduct);
-product_route.delete('/delete/:_id',auth,product_controller.deleteProduct);
+product_route.post('/add-product',product_controller.add_product);
+product_route.get('/get-products',product_controller.getProducts);
+product_route.get('/product/:productId',product_controller.getProduct);
+product_route.put('/update/:_id',product_controller.updateProduct);
+product_route.delete('/delete/:_id',product_controller.deleteProduct);
 
 
 module.exports = product_route;
